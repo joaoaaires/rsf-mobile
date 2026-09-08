fa02e68005f0fe7c621a807b13bc3cf8eac3a11f
Joao <jantonioayres@icloud.com>
2026-09-08 00:50:50 -0300

    feat: init splash

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,26 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { Text, View, Image, ActivityIndicator } from "react-native";
+
+const LOGO = require("@/assets/images/logo/logo.png");
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-red-500">
-        Welcome to Nativewind!
+    <View className="flex-1 items-center justify-center gap-6 bg-[#0b0b10]">
+      <Image className="h-32" source={LOGO} resizeMode="center" />
+      <View className="justify-center items-center">
+        <Text className="text-xl font-normal text-white">
+          Rádio São Francisco
+        </Text>
+        <Text className="text-xl font-bold text-white">FM</Text>
+      </View>
+      <Text className="text-2xl font-bold text-white">87,9 FM</Text>
+      <Text className="text-2xl text-[#a8a8b8FF]">
+        A rádio da comunidade!
       </Text>
-      <StatusBar style="auto" />
+      <View className="mt-20">
+        <ActivityIndicator size={32} color="#ff2b31FF" />
+      </View>
+      <StatusBar style="light" />
     </View>
   );
 }
